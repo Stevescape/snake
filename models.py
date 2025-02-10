104278e7860c5818d3fac521bc8b8f07756d1b2c
Steven Truong <skolton09@gmail.com>
2025-02-10 00:35:44 -0700

    FULLY FUNCTIONING SNAKE

--- models.py
+++ models.py
@@ -1,9 +1,19 @@
 import pygame
+import random
 
 NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
+BLOCKSIZE = 20
+
+def collided(ent1, ent2):
+    return (
+        ent1.x < ent2.x + BLOCKSIZE and
+        ent1.x + BLOCKSIZE > ent2.x and
+        ent1.y < ent2.y + BLOCKSIZE and
+        ent1.y + BLOCKSIZE > ent2.y
+    )
 
 class Player:
     def __init__(self, screenX, screenY):
@@ -19,28 +29,59 @@ class Player:
     def getHead(self):
         return self.head
     
-    def addBlock(self, x, y, dir):
-        new = Block()
-        new.setX(x)
-        new.setY(y)
-        new.setDir(dir)
-        self.head.addToTail(new)
+    def addBlock(self):
+        self.head.addToTail()
 
     def renderPlayer(self, screen):
-        self.head.renderBlock(screen)
+        self.head.renderBlock(screen, (0, 255, 255))
+
+    def updatePos(self):
+        self.head.updatePos()
+
+    def updateDir(self, dir):
+        self.head.setDir(dir)
+
+    def collidedWithSelf(self):
+        return self.collideTail(self.head.tail)
+    
+    def collideTail(self, tail):
+        if tail == None:
+            return False
+
+        if collided(self.head, tail):
+            return True
+        else:
+            return self.collideTail(tail.tail)
         
         
+class Pellet:
+    def __init__(self, width, height):
+        self.x = random.randint(0, width)
+        self.y = random.randint(0, height)
+        
+        self.x = self.x - (self.x % 30)
+        self.y = self.y - (self.y % 30)
+
+        
+    def renderPellet(self, screen):
+        rect = (self.x, self.y, BLOCKSIZE, BLOCKSIZE)
+        pygame.draw.rect(screen, (255, 255, 255), rect, 0)
+
 
 class Block:
     def __init__(self):
         self.tail = None
         self.x = None
         self.y = None
+        self.speed = BLOCKSIZE+10
         self.direction = NORTH
     
     def setTail(self, node):
         self.tail = node
     
+    def setSpeed(self, speed):
+        self.speed = speed
+
     def setX(self, xpos):
         self.x = xpos
 
@@ -50,14 +91,46 @@ class Block:
     def setDir(self, direction):
         self.direction = direction
 
-    def addToTail(self, block):
+    def addToTail(self):
         if (self.tail == None):
-            self.tail = block
+            new = Block()
+            diff = BLOCKSIZE + 10
+            if self.direction == NORTH:
+                new.setX(self.x)
+                new.setY(self.y + diff)
+            elif self.direction == EAST:
+                new.setX(self.x - diff)
+                new.setY(self.y)
+            elif self.direction == SOUTH:
+                new.setX(self.x)
+                new.setY(self.y - diff)
+            else:
+                new.setX(self.x + diff)
+                new.setY(self.y)
+            new.setDir(self.direction)
+            self.tail = new
             return
-        self.addToTail(self.tail)
+        self.tail.addToTail()
 
-    def renderBlock(self, screen):
-        rect = (self.x, self.y, 20, 20)
-        pygame.draw.rect(screen, (0, 255, 0), rect, 0)
+    def renderBlock(self, screen, color):
+        rect = (self.x, self.y, BLOCKSIZE, BLOCKSIZE)
+        pygame.draw.rect(screen, color, rect, 0)
         if self.tail != None:
-            self.tail.renderBlock(screen)
+            self.tail.renderBlock(screen, (0, 255, 0))
+
+    def updatePos(self):
+        if self.tail != None:
+            self.tail.updatePos()
+
+        if self.direction == NORTH:
+            self.setY(self.y - self.speed)
+        elif self.direction == EAST:
+            self.setX(self.x + self.speed)
+        elif self.direction == SOUTH:
+            self.setY(self.y + self.speed)
+        else:
+            self.setX(self.x - self.speed)
+        
+        if self.tail != None:
+            self.tail.setDir(self.direction)
+        
--- snake.py
+++ snake.py
@@ -5,32 +5,105 @@ NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
+BLOCKSIZE = models.BLOCKSIZE
+
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 720
 
 pygame.init()
-screen = pygame.display.set_mode((720, 720))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 keepRunning = True
 player = models.Player(pygame.display.get_window_size()[0], pygame.display.get_window_size()[1])
+pellet = models.Pellet(SCREEN_WIDTH, SCREEN_HEIGHT)
+score = 0
+gameOver = False
 
-def renderPlayer():
-    player.renderPlayer(screen)
+def renderScore():
+    font = pygame.font.Font('freesansbold.ttf', 10)
+    text = font.render(f'Score {score}', True, (0, 255, 0), (0, 0, 255))
+    textRect = text.get_rect()
+    textRect.center = (25, 15)
+    screen.blit(text, textRect)
 
 def renderGame():
-    renderPlayer()
+    # Render Player
+    player.renderPlayer(screen)
+    # Render Pellet
+    pellet.renderPellet(screen)
+    renderScore()
+
+def outOfBounds(ent1):
+    return (
+        ent1.x < 0 or 
+        ent1.x + BLOCKSIZE > SCREEN_WIDTH or 
+        ent1.y < 0 or 
+        ent1.y + BLOCKSIZE > SCREEN_HEIGHT
+    )
+
+def updateGame():
+    global gameOver
+    global pellet
+    global score
+    player.updatePos()
+
+    if models.collided(player.getHead(), pellet):
+        player.addBlock()
+        pellet = models.Pellet(SCREEN_WIDTH, SCREEN_HEIGHT)
+        score += 1
+
+    if player.collidedWithSelf() or outOfBounds(player.getHead()):
+        gameOver = True
+
+def renderGameOver():
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    text = font.render('Game Over (Press R to Reset)', True, (0, 255, 0), (0, 0, 255))
+    textRect = text.get_rect()
+    textRect.center = (pygame.display.get_window_size()[0] // 2, pygame.display.get_window_size()[1] // 2)
+    screen.blit(text, textRect)
+    renderScore()
+
+def resetGame():
+    global gameOver
+    global player
+    global pellet
+    global score
+    gameOver = False
+    player = models.Player(pygame.display.get_window_size()[0], pygame.display.get_window_size()[1])
+    pellet = models.Pellet(SCREEN_WIDTH, SCREEN_HEIGHT)
+    score = 0
+
+def processKeyEvent(key):
+    if key == pygame.K_a:
+        player.updateDir(WEST)
+    elif key == pygame.K_w:
+        player.updateDir(NORTH)
+    elif key == pygame.K_d:
+        player.updateDir(EAST)
+    elif key == pygame.K_s:
+        player.updateDir(SOUTH)
+    elif key == pygame.K_r:
+        resetGame()
 
 while keepRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keepRunning = False
+        if event.type == pygame.KEYDOWN:
+            processKeyEvent(event.key)
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    renderGame()
+    if not gameOver:
+        renderGame()
+        updateGame()
+    else:
+        renderGameOver()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(10)  # limits FPS to 60
 
 pygame.quit()
